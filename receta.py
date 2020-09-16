dae2924bb67b0432ddd3414eb1d1868c7e6e92b6
Mariano Dubois <nariuk95@gmail.com>
2020-09-16 12:29:21 -0300

    archivos 16 sep

--- receta.py
+++ receta.py
@@ -1,7 +1,18 @@
-import sys
+import sys, os
 from PySide2.QtCore import Slot
 from PySide2.QtWidgets import QApplication, QMainWindow
 from principal import Ui_Buscardor
+from resultado import Ui_Resultado
+
+
+class Resultado(QMainWindow):
+    def __init__(self):
+        super(Resultado, self).__init__()
+        self.ui = Ui_Resultado()
+        self.ui.setupUi(self)
+
+    def cerrar(self):
+        self.close()
 
 class Principal(QMainWindow):
     def __init__(self):
@@ -10,9 +21,22 @@ class Principal(QMainWindow):
         self.ui.setupUi(self)
         self.opened_file = None
 
-    @Slot()
-    def func(self):
-        pass
+
+    def buscar(self):
+        self.nombre = self.ui.lblNombre.text()
+        self.user = os.popen("whoami").read()
+        self.user = self.user.rsplit()
+        self.ruta = "/home/"+self.user[0]
+        os.chdir(self.ruta)
+        self.resultado = os.popen("find -not -path '*/\.*' | grep '" + self.nombre + "'").read()
+        print(self.resultado)
+        self.ventanita = Resultado()
+        self.ventanita.ui.txtResultado.setText(self.resultado)
+        self.ventanita.show()
+
+    def borrar(self):
+        self.ui.lblNombre.setTxt("")
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
